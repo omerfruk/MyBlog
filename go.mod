5df4531b1049b99c5f3f49d9b05d4e998625bee9
omerfruk <omer.fruk3547@gmail.com>
2021-03-09 17:36:53 +0300

    jwt için token olusturuldu

--- go.mod
+++ go.mod
@@ -3,12 +3,12 @@ module github.com/omerfruk/my-blog
 go 1.15
 
 require (
-	github.com/gofiber/fiber v1.14.6
 	github.com/gofiber/fiber/v2 v2.5.0
 	github.com/gofiber/template v1.6.6
 	github.com/jackc/pgproto3/v2 v2.0.7 // indirect
-	golang.org/x/crypto v0.0.0-20210220033148-5ea612d1eb83 // indirect
-	golang.org/x/text v0.3.5 // indirect
+	github.com/klauspost/compress v1.11.12 // indirect
+	github.com/valyala/fasthttp v1.22.0 // indirect
+	golang.org/x/sys v0.0.0-20210309074719-68d13333faf2 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	gorm.io/driver/postgres v1.0.8
 	gorm.io/gorm v1.20.12
--- handlers/researcher.go
+++ handlers/researcher.go
@@ -2,13 +2,12 @@ package handlers
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/session"
 	"github.com/omerfruk/my-blog/database"
 	"github.com/omerfruk/my-blog/models"
 	"github.com/omerfruk/my-blog/service"
-	"gorm.io/gorm"
 	"io/ioutil"
 	"os"
 	"strings"
@@ -65,79 +64,119 @@ func ResearchersAllJson(c *fiber.Ctx) error {
 	})
 
 }
-//login sayfasının renderi
 
+//login sayfasının renderi
 
 func Login(c *fiber.Ctx) error {
 
 	return c.Render("login", true)
 }
+
 //gingup render
 func SingUp(c *fiber.Ctx) error {
 
 	return c.Render("singup", true)
 }
+
 //sing up post render
-func SingUpPost(c *fiber.Ctx)  error {
+func SingUpPost(c *fiber.Ctx) error {
 	var request models.RequestSingUp
 	err := c.BodyParser(&request)
 	if err != nil {
 		fmt.Println(err)
 	}
-	service.SingUPUser(request.Phone,request.FullName,request.Email,request.Password)
+	service.SingUPUser(request.Phone, request.FullName, request.Email, request.Password)
 	return c.Redirect("/login")
 }
 
 func LogControl(c *fiber.Ctx) error {
 	var request models.RequestBody
 	var temp models.User
+
 	err := c.BodyParser(&request)
 	if err != nil {
 		fmt.Println(err)
 	}
-	//fmt.Println(request.Email + "   " + request.Password)
-	err=database.DB().Where("mail = ? and password = ?", request.Email, request.Password).First(&temp).Error
-	if err !=nil{
-		if(errors.Is(err,gorm.ErrRecordNotFound)){
-			fmt.Println("boyle bi uye yok")
-			return c.Redirect("/")
-		}
+	err = database.DB().Where("mail = ? and password = ?", request.Email, request.Password).First(&temp).Error
+	if err != nil {
+		return c.SendStatus(fiber.StatusUnauthorized)
 	}
 	return c.Redirect("/admin")
 }
 
-func AdminPage(c *fiber.Ctx) error{
-	var temp []models.User
-	err:=database.DB().Find(&temp).Error
+// jwt token olusturuldu uretimi
+/*func LogControl(c *fiber.Ctx) error {
+	var request models.RequestBody
+	var temp models.User
+	app := fiber.New()
+	app.Use(jwtware.New(jwtware.Config{
+		SigningKey: []byte("secret"),
+	}))
+	err := c.BodyParser(&request)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(request.Email + "   " + request.Password)
+	err = database.DB().Where("mail = ? and password = ?", request.Email, request.Password).First(&temp).Error
+	if err != nil {
+		return c.SendStatus(fiber.StatusUnauthorized)
+	}
+	fmt.Println(temp.Fullname)
+	fmt.Println(temp.Authority)
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	claims:=token.Claims.(jwt.MapClaims)
+	claims["name"]=temp.Fullname
+	claims["admin"]=temp.Authority
+	claims["exp"]=time.Now().Add(time.Hour * 24 * 30).Unix()
+
+	fmt.Println(claims)
+
+	t,err := token.SignedString([]byte("secret"))
 	if err != nil{
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+	return c.JSON(fiber.Map{
+		"token":t,
+	})
+}
+func Restricted(c *fiber.Ctx) error {
+	user := c.Locals("user").(*jwt.Token)
+	claims := user.Claims.(jwt.MapClaims)
+	name := claims["name"].(string)
+	return c.SendString("Welcome " + name)
+}*/
+
+func AdminPage(c *fiber.Ctx) error {
+	var temp []models.User
+	err := database.DB().Find(&temp).Error
+	if err != nil {
 		fmt.Println(err)
 	}
 	//fmt.Println(temp)
-	return c.Render("admin",temp)
+	return c.Render("admin", temp)
 }
 
 func EditUser(c *fiber.Ctx) error {
-	key:=c.Params("key")
+	key := c.Params("key")
 	var request models.RequestSingUp
 
 	err := c.BodyParser(&request)
 	if err != nil {
 		fmt.Println(err)
 	}
-	service.UpdateUser(key,request.Email,request.FullName,request.Phone)
- return c.Redirect("/admin")
+	service.UpdateUser(key, request.Email, request.FullName, request.Phone)
+	return c.Redirect("/admin")
 }
 
-func DltUSer(c *fiber.Ctx)error {
-	key:=c.Params("key")
+func DltUSer(c *fiber.Ctx) error {
+	key := c.Params("key")
 	fmt.Println(key)
 
 	service.DeleteUser(key)
 
 	return c.Redirect("/admin")
 }
-
-
 
 func IndexRender(c *fiber.Ctx) error {
 	topbar := service.GetTopBar("ÖmFar.")
@@ -168,4 +207,31 @@ func InfoRender(c *fiber.Ctx) error {
 	}
 	fmt.Println(I.Footer.InstaSrc)
 	return c.Render("info", I)
+}
+
+func Session(c *fiber.Ctx) error {
+	store := session.New()
+	sess, err := store.Get(c)
+	if err != nil {
+		panic(err)
+	}
+
+	// save session
+	defer sess.Save()
+
+	// Get value
+	//name := sess.Get("name")
+
+	// Set key/value
+	sess.Set("name", "john")
+
+	// Delete key
+	sess.Delete("name")
+
+	// Destroy session
+	if err := sess.Destroy(); err != nil {
+		panic(err)
+	}
+	fmt.Println(sess)
+	return c.Render("/", true)
 }
