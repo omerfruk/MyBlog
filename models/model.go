426589befc836ab2949f8ca8d47ba7427b9f01bf
omerfruk <omer.fruk3547@gmail.com>
2021-03-09 17:35:30 +0300

    sessision icin yeni struct olusturuldu

--- models/model.go
+++ models/model.go
@@ -1,7 +1,9 @@
 package models
 
 import (
+	"github.com/gofiber/fiber/v2"
 	"gorm.io/gorm"
+	"time"
 )
 
 type Topbar struct {
@@ -101,4 +103,42 @@ type User struct {
 	Fullname    string    `json:"fullname"`
 	Information string    `json:"information"`
 	Authority   Authority `json:"authority"`
+}
+
+type Config struct {
+	// Allowed session duration
+	// Optional. Default value 24 * time.Hour
+	Expiration time.Duration
+
+	// Storage interface to store the session data
+	// Optional. Default value memory.New()
+	Storage fiber.Storage
+
+	// Name of the session cookie. This cookie will store session key.
+	// Optional. Default value "session_id".
+	CookieName string
+
+	// Domain of the CSRF cookie.
+	// Optional. Default value "".
+	CookieDomain string
+
+	// Path of the CSRF cookie.
+	// Optional. Default value "".
+	CookiePath string
+
+	// Indicates if CSRF cookie is secure.
+	// Optional. Default value false.
+	CookieSecure bool
+
+	// Indicates if CSRF cookie is HTTP only.
+	// Optional. Default value false.
+	CookieHTTPOnly bool
+
+	// Indicates if CSRF cookie is HTTP only.
+	// Optional. Default value false.
+	CookieSameSite string
+
+	// KeyGenerator generates the session key.
+	// Optional. Default value utils.UUID
+	KeyGenerator func() string
 }
