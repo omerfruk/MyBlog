47940f9f5a66a2e1979b2eccdb0fe8164ee4aa45
omerfruk <omer.fruk3547@gmail.com>
2021-03-10 11:17:18 +0300

    models tekli gereksiz postgreSQl temizlendi

--- models/model.go
+++ models/model.go
@@ -2,50 +2,7 @@ package models
 
 import (
 	"gorm.io/gorm"
-	"time"
 )
-
-type PostgreSql struct {
-	// Host name where the DB is hosted
-	//
-	// Optional. Default is "127.0.0.1"
-	Host string
-
-	// Port where the DB is listening on
-	//
-	// Optional. Default is 5432
-	Port int
-
-	// Server username
-	//
-	// Optional. Default is ""
-	Username string
-
-	// Server password
-	//
-	// Optional. Default is ""
-	Password string
-
-	// Database name
-	//
-	// Optional. Default is "fiber"
-	Database string
-
-	// Table name
-	//
-	// Optional. Default is "fiber_storage"
-	Table string
-
-	// Reset clears any existing keys in existing Table
-	//
-	// Optional. Default is false
-	Reset bool
-
-	// Time before deleting expired keys
-	//
-	// Optional. Default is 10 * time.Second
-	GCInterval time.Duration
-}
 
 type Topbar struct {
 	gorm.Model
