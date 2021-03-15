b55fb00f1eae8e77aed45d4906ff1c6c1c52220c
omerfruk <omer.fruk3547@gmail.com>
2021-03-15 16:23:09 +0300

    error handlering hatası düzeltildi

--- service/entry_service.go
+++ service/entry_service.go
@@ -13,31 +13,31 @@ func CreateEntry(img string, header string, text string, buton string) {
 		Text:       text,
 		ButtonText: buton,
 	}
-	if tempCont:= database.DB().Where("header = ? and text = ?" ,temp.Header,temp.Text ).First(&temp);tempCont.Error != nil{
-		err:=database.DB().Create(&temp)
-		if err !=nil{
+	if tempCont := database.DB().Where("header = ? and text = ?", temp.Header, temp.Text).First(&temp); tempCont.Error != nil {
+		err := database.DB().Create(&temp)
+		if err != nil {
 			fmt.Println(err)
 		}
 	}
 }
-func GetEntry(header string)models.Entry{
+func GetEntry(header string) models.Entry {
 	var entry models.Entry
-	err:=database.DB().Where("header = ?",header).First(&entry)
-	if err !=nil{
+	err := database.DB().Where("header = ?", header).First(&entry).Error
+	if err != nil {
 		fmt.Println(err)
 	}
 	return entry
 }
-func DeleteEntry(header string)  {
-	temp:=new(models.Entry)
+func DeleteEntry(header string) {
+	temp := new(models.Entry)
 	database.DB().Where("header = ?", header).Delete(&temp)
 }
-func UpdateEntry(oldHeader string,img string,header string,text string,button string)  {
+func UpdateEntry(oldHeader string, img string, header string, text string, button string) {
 	var temp models.Entry
-	database.DB().Where("header = ?",header).Find(&temp)
-	temp.ImgSrc=img
-	temp.Header=header
-	temp.Text=text
-	temp.ButtonText=button
+	database.DB().Where("header = ?", header).Find(&temp)
+	temp.ImgSrc = img
+	temp.Header = header
+	temp.Text = text
+	temp.ButtonText = button
 	database.DB().Save(&temp)
 }
