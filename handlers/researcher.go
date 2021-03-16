fe65cb921d90270df2f94c2a857087ef2f76034d
omerfruk <omer.fruk3547@gmail.com>
2021-03-16 09:48:11 +0300

    admin sayfasina login olmadan giris engellendi

--- handlers/researcher.go
+++ handlers/researcher.go
@@ -210,17 +210,27 @@ func Restricted(c *fiber.Ctx) error {
 
 //admin sayfasi
 func AdminPage(c *fiber.Ctx) error {
-	var user []models.User
-	inf := service.GetUser("omer faruk")
-	topbar := service.GetTopBar("ÖmFar.")
-	err := database.DB().Find(&user).Error
+	var admin models.AdminPage
+	sess, err := store.Get(c)
 	if err != nil {
 		fmt.Println(err)
 	}
-	admin := models.AdminPage{
-		Topbar: topbar,
-		Info:   inf,
-		User:   user,
+	if sess.Get("temp") == nil {
+		c.Redirect("/login")
+	} else {
+		var user []models.User
+		inf := service.GetUser("omer faruk")
+		topbar := service.GetTopBar("ÖmFar.")
+		err = database.DB().Find(&user).Error
+		if err != nil {
+			fmt.Println(err)
+		}
+		admin = models.AdminPage{
+			Topbar: topbar,
+			Info:   inf,
+			User:   user,
+		}
+		return c.Render("admin", admin)
 	}
 	return c.Render("admin", admin)
 }
