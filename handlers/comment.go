0e8aebf7b21d12f1c9700f1c8967710bcc48d112
omerfruk <omer.fruk3547@gmail.com>
2021-03-19 15:48:29 +0300

    comment sayfasi ayri yere tasindi

---
+++ handlers/comment.go
@@ -0,0 +1,30 @@
+package handlers
+
+import (
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"github.com/omerfruk/my-blog/models"
+	"github.com/omerfruk/my-blog/service"
+)
+
+//dusunceler sayfasi
+func Comments(c *fiber.Ctx) error {
+	temp := service.GetAllComments()
+	return c.Render("comments", temp)
+}
+
+//dusunce sayfasi get
+func Comment(c *fiber.Ctx) error {
+	return c.Render("comment", true)
+}
+
+//dusunce sayfasi olusturmna
+func CommentCreate(c *fiber.Ctx) error {
+	var temp models.Comment
+	err := c.BodyParser(&temp)
+	if err != nil {
+		fmt.Println(err)
+	}
+	service.CommentCreate(temp.Fullname, temp.Phone, temp.Mail, temp.Title, temp.Text, "")
+	return c.Redirect("/comment")
+}
