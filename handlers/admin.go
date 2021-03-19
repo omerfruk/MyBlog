f46e2847a478b3a90696aab76cd09b472e297e80
omerfruk <omer.fruk3547@gmail.com>
2021-03-19 15:48:22 +0300

    admin sayfasi ayri yere tasindi

---
+++ handlers/admin.go
@@ -0,0 +1,37 @@
+package handlers
+
+import (
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"github.com/omerfruk/my-blog/database"
+	"github.com/omerfruk/my-blog/models"
+	"github.com/omerfruk/my-blog/service"
+)
+
+//admin sayfasi
+func AdminPage(c *fiber.Ctx) error {
+	var admin models.AdminPage
+	sess, err := store.Get(c)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(sess.Get("temp"))
+	if sess.Get("temp") == nil {
+		c.Redirect("/login")
+	} else {
+		var user []models.User
+		inf := service.GetUser("omer faruk")
+		topbar := service.GetTopBar("ÖmFar.")
+		err = database.DB().Find(&user).Error
+		if err != nil {
+			fmt.Println(err)
+		}
+		admin = models.AdminPage{
+			Topbar: topbar,
+			Info:   inf,
+			User:   user,
+		}
+		return c.Render("admin", admin)
+	}
+	return c.Render("admin", admin)
+}
