12dc4fb694ee28005890688357d8b825971a56f1
omerfruk <omer.fruk3547@gmail.com>
2021-03-22 17:50:07 +0300

    method eklendi

--- service/gallery_service.go
+++ service/gallery_service.go
@@ -9,6 +9,12 @@ import (
 	"github.com/omerfruk/my-blog/models"
 )
 
+func GetFotoAll() []models.FotoGallery {
+	var temp []models.FotoGallery
+	database.DB().Find(&temp)
+	return temp
+}
+
 func CreateFoto(gallery models.FotoGallery) {
 	err := database.DB().Where("img_src = ? ", gallery.ImgSrc).First(&gallery).Error
 	if errors.Is(err, gorm.ErrRecordNotFound) {
@@ -21,8 +27,18 @@ func CreateFoto(gallery models.FotoGallery) {
 	}
 }
 
-func GetFotoAll() []models.FotoGallery {
-	var temp []models.FotoGallery
-	database.DB().Find(&temp)
+func UpdateFoto(img string, gallery models.FotoGallery) models.FotoGallery {
+	var temp models.FotoGallery
+	err := database.DB().Where("img_src = ?", img).Find(&temp).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		fmt.Println("boyle bir resim yok")
+		return gallery
+	}
+	temp = gallery
 	return temp
+}
+
+func DeleteFoto(img string) {
+	var temp models.FotoGallery
+	database.DB().Where("img_src = ?", img).Delete(&temp)
 }
