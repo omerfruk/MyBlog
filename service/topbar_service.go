32ff47ffbab73a9a09d986a83e29088c019a9d5d
omerfruk <omer.fruk3547@gmail.com>
2021-03-22 14:16:01 +0300

    topbar servisleriguncellendi

--- service/topbar_service.go
+++ service/topbar_service.go
@@ -1,23 +1,24 @@
 package service
 
 import (
+	"errors"
 	"fmt"
 	"github.com/omerfruk/my-blog/database"
 	"github.com/omerfruk/my-blog/models"
+	"gorm.io/gorm"
 )
 
-func CreateTopBar(logo string, home string, future string, port string, contact string,log string) {
-	temp := models.Topbar{
-		Logo:    logo,
-		Home:    home,
-		Future:  future,
-		Port:    port,
-		Contact: contact,
-		Login: log,
+func CreateTopBar(topbar models.Topbar) {
+	err := database.DB().Where("Logo = ? ", topbar.Logo).First(&topbar).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		err = database.DB().Create(&topbar).Error
+		if err != nil {
+			fmt.Println(err)
+		}
+	} else {
+		fmt.Println("boyle bir tobbar mevcud")
 	}
-	if tempCont := database.DB().Where("Logo = ? ", temp.Logo).First(&temp); tempCont.Error != nil {
-		database.DB().Create(&temp)
-	}
+
 }
 func DeleteTopBar(logo string) {
 	var temp models.Topbar
@@ -34,16 +35,15 @@ func GetTopBar(logo string) models.Topbar {
 
 }
 
-func UpdateTopBar(oldLogo string,logo string, home string, future string, port string, contact string,log string)  {
+func UpdateTopBar(oldLogo string, logo string, home string, future string, port string, contact string, log string) {
 	var temp models.Topbar
-	database.DB().Where("logo = ?",oldLogo).Find(&temp)
-
-	temp.Logo=logo
-	temp.Home=home
-	temp.Future=future
-	temp.Port=port
-	temp.Contact=contact
-	temp.Login=logo
+	database.DB().Where("logo = ?", oldLogo).Find(&temp)
+	temp.Logo = logo
+	temp.Home = home
+	temp.Future = future
+	temp.Port = port
+	temp.Contact = contact
+	temp.Login = logo
 
 	database.DB().Save(&temp)
 }
