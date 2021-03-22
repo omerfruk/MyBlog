2cf1d44d8dbcfb36e993b870c049923a06aff40b
omerfruk <omer.fruk3547@gmail.com>
2021-03-22 14:46:04 +0300

    gallery get foto all  methodu duzeltildi

--- service/gallery_service.go
+++ service/gallery_service.go
@@ -1,18 +1,28 @@
 package service
 
 import (
+	"errors"
+	"fmt"
+	"gorm.io/gorm"
+
 	"github.com/omerfruk/my-blog/database"
 	"github.com/omerfruk/my-blog/models"
 )
 
-func CreateFoto(imgsrc string, title string, text string) {
-	temp := models.FotoGallery{
-		ImgSrc: imgsrc,
-		Title:  title,
-		Text:   text,
+func CreateFoto(gallery models.FotoGallery) {
+	err := database.DB().Where("img_src = ? ", gallery.ImgSrc).First(&gallery).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		err = database.DB().Create(&gallery).Error
+		if err != nil {
+			fmt.Println(err)
+		}
+	} else {
+		fmt.Println("boyle uzantıda bir resim bulunmaktadir")
 	}
-	if tempFoto := database.DB().Where("img_src = ? ", imgsrc).First(&temp); tempFoto.Error != nil {
-		database.DB().Create(&temp)
-	}
+}
 
+func GetFotoAll() []models.FotoGallery {
+	var temp []models.FotoGallery
+	database.DB().Find(&temp)
+	return temp
 }
