a3088c22562ae91bcf188dd9daf175db57f2cd59
omerfruk <omer.fruk3547@gmail.com>
2021-03-22 14:22:46 +0300

    resercher methodu daha guzel hala getirildi

--- service/researcher_service.go
+++ service/researcher_service.go
@@ -1,40 +1,45 @@
 package service
 
 import (
+	"errors"
 	"fmt"
 	"github.com/omerfruk/my-blog/database"
 	"github.com/omerfruk/my-blog/models"
+	"gorm.io/gorm"
 )
-func GetResearch(area string) [] models.Research{
-	var research [] models.Research
-	err := database.DB().Where("area = ?", area ).Find(&research).Error
-	if err != nil{
+
+func GetResearch(area string) []models.Research {
+	var research []models.Research
+	err := database.DB().Where("area = ?", area).Find(&research).Error
+	if err != nil {
 		fmt.Println(err)
 	}
 	return research
 }
 
-func CreateResearch(area string, title string, text string) {
+func CreateResearch(research models.Research) {
+	err := database.DB().Where("title=? and text=?", research.Title, research.Text).First(&research).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		err = database.DB().Create(&research).Error
+		if err != nil {
+			fmt.Println(err)
+		}
+	} else {
+		fmt.Println("boyle bir arastirma bulunuyor")
+	}
+
+}
+
+func DeleteResearch(title string) {
 	temp := new(models.Research)
+	database.DB().Where("title=?", title).Delete(&temp)
+}
+
+func UpdateResarch(id int, area string, title string, text string) {
+	temp := new(models.Research)
+	database.DB().Where("id=?", id).Find(&temp)
 	temp.Area = area
 	temp.Text = text
 	temp.Title = title
-
-	if researchTemp := database.DB().Where("title=? and text=?", temp.Title, temp.Text).First(&temp); researchTemp.Error != nil {
-		database.DB().Create(&temp)
-	}
-}
-
-func DeleteResearch(title string)  {
-	temp:=new(models.Research)
-	database.DB().Where("title=?",title).Delete(&temp)
-}
-
-func UpdateResarch(id int,area string,title string, text string)  {
-	temp:=new(models.Research)
-	database.DB().Where("id=?",id).Find(&temp)
-	temp.Area=area
-	temp.Text=text
-	temp.Title=title
 	database.DB().Save(&temp)
 }
