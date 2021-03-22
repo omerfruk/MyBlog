e5eea77ca60dedf8c75c18c9986772bafb7ddd2a
omerfruk <omer.fruk3547@gmail.com>
2021-03-22 14:15:33 +0300

    user servisler guncellendi

--- service/user_service.go
+++ service/user_service.go
@@ -1,24 +1,22 @@
 package service
 
 import (
+	"errors"
 	"fmt"
 	"github.com/omerfruk/my-blog/database"
 	"github.com/omerfruk/my-blog/models"
 	"gorm.io/gorm"
 )
 
-type userService struct {
-	db *gorm.DB
-}
-
-func GetUser(name string) models.User {
+func GetUser(id string) models.User {
 	var user models.User
-	err := database.DB().Where("authority=?", 2).First(&user).Error
+	err := database.DB().Where("id = ?", id).First(&user).Error
 	if err != nil {
 		fmt.Println(err)
 	}
 	return user
 }
+
 func GetUserAll() models.User {
 	var user models.User
 	err := database.DB().First(&user).Error
@@ -27,48 +25,40 @@ func GetUserAll() models.User {
 	}
 	return user
 }
-func SingUPUser(phone string, name string, email string, pass string, img string) {
-	password := Sha256String(pass)
-	temp := new(models.User)
-	temp.Information = phone
-	temp.Fullname = name
-	temp.ImgSrc = img
-	temp.Mail = email
-	temp.Password = password
-	temp.Authority = models.Basic
-	if UserTemp := database.DB().Where("email=?", temp.Mail).First(&temp); UserTemp.Error != nil {
-		database.DB().Create(&temp)
+
+func GetAdmin(name string) models.User {
+	var temp models.User
+	err := database.DB().Where("fullname = ? and authority = ?", name, models.Admin).Find(&temp).Error
+	if err != nil {
+		fmt.Println(err)
 	}
+	return temp
 }
-func CreateUser(img string, header string, name string, mail string, info string, isAdmin bool) {
-	temp := new(models.User)
-	temp.Header = header
-	temp.ImgSrc = img
-	temp.Mail = mail
-	temp.Fullname = name
-	temp.Information = info
+
+func CreateUser(user models.User, isAdmin bool) {
 	if isAdmin == true {
-		temp.Authority = models.Admin
+		user.Authority = models.Admin
 	} else {
-		temp.Authority = models.Basic
+		user.Authority = models.Basic
 	}
-	if UserTemp := database.DB().Where("fullname=?", temp.Fullname).First(&temp); UserTemp.Error != nil {
-		database.DB().Create(&temp)
+	user.Password = Sha256String(user.Password)
+	err := database.DB().Where("mail=?", user.Mail).First(&user).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		err = database.DB().Create(&user).Error
+		if err != nil {
+			fmt.Println(err)
+		}
+	} else {
+		fmt.Println("bu maile ait bir hesab zaten var")
 	}
+
 }
 func DeleteUser(id string) {
 	temp := new(models.User)
 	database.DB().Where("id=?", id).Delete(&temp)
 }
-func UpdateUser(id uint, mail string, name string, info string, img string) {
 
-	var temp models.User
-
-	database.DB().Where("id=?", id).Find(&temp)
-	temp.Information = info
-	temp.Fullname = name
-	temp.ImgSrc = img
-	temp.Mail = mail
-	database.DB().Save(&temp)
-
+func UpdateUser(user models.User, Olduser models.User) {
+	Olduser = user
+	database.DB().Save(&Olduser)
 }
