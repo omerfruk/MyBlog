f32dd308416b68cc6c7dbc299b303027735fa4ec
omerfruk <omer.fruk3547@gmail.com>
2021-03-22 14:30:28 +0300

    instructions create methodu duzeltildi

--- service/ınstructions_service.go
+++ service/ınstructions_service.go
@@ -1,24 +1,23 @@
 package service
 
 import (
+	"errors"
 	"fmt"
+	"gorm.io/gorm"
+
 	"github.com/omerfruk/my-blog/database"
 	"github.com/omerfruk/my-blog/models"
 )
 
-func CreateInstructions(title string, info string, LeftOpt string, MidOpt string, RghtOpt string, LeftDesc string, MidDesc string, RghtDesc string) {
-	temp := models.Instructions{
-		Title:     title,
-		Info:      info,
-		LeftIntro: LeftOpt,
-		MidIntro:  MidOpt,
-		RghtIntro: RghtOpt,
-		LeftDesc:  LeftDesc,
-		MidDesc:   MidDesc,
-		RghtDesc:  RghtDesc,
-	}
-	if tempCont := database.DB().Where("title = ? and info = ?", temp.Title, temp.Info).First(&temp); tempCont.Error != nil {
-		database.DB().Create(&temp)
+func CreateInstructions(instructions models.Instructions) {
+	err := database.DB().Where("title = ? and info = ?", instructions.Title, instructions.Info).First(&instructions).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		err = database.DB().Create(&instructions).Error
+		if err != nil {
+			fmt.Println(err)
+		}
+	} else {
+		fmt.Println("boyle bir giris bulunmaktadir")
 	}
 }
 func DeleteInstructions(title string) {
