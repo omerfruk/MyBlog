0b38ae8f3ae335c4b41ea70dc8ffe99952c1f175
omerfruk <omer.fruk3547@gmail.com>
2021-03-23 18:39:04 +0300

    admin guruplanmasi yapildi

--- routers/router.go
+++ routers/router.go
@@ -1,6 +1,7 @@
 package routers
 
 import (
+	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/omerfruk/my-blog/handlers"
 )
@@ -10,25 +11,18 @@ func Router(app *fiber.App) {
 	app.Static("/", "./")
 	app.Get("/", handlers.IndexRender)
 	app.Get("/info", handlers.InfoRender)
+
 	app.Get("/login", handlers.Login)
 	app.Post("/login", handlers.LogControl)
 	app.Get("/logout", handlers.Logout)
 	app.Get("/singup", handlers.SingUp)
 	app.Post("/singup", handlers.SingUpPost)
-	app.Get("/admin", handlers.AdminPage)
 	app.Get("/down", handlers.DownPage)
-	app.Post("/delete/:key", handlers.DeletUser)
-	app.Post("/edit/:key", handlers.UpdateUser)
-	app.Post("/create", handlers.CreateUser)
+
 	app.Get("/gallery", handlers.Gallery)
-	app.Get("/galleryAdd", handlers.GalleryAddGet)
-	app.Post("/galleryAdd", handlers.GalleryAddPost)
+
 	app.Get("/comments", handlers.Comments)
-	app.Get("/comment", handlers.Comment)
-	app.Post("/comment", handlers.CommentCreate)
-	app.Get("/user/:key", handlers.GetUser)
-	app.Post("/user/update/:key", handlers.UpdateUser)
-	app.Get("/user/delete/:key", handlers.DeletUser)
+
 	app.Get("/users", handlers.GetUsers)
 
 	res := app.Group("/researcher/:key", handlers.ResearchersAll) // /researcher
@@ -38,5 +32,29 @@ func Router(app *fiber.App) {
 	res.Get("/Developer", handlers.ResearchersAll)                // /researcher/Developer
 	res.Get("/Hardware", handlers.ResearchersAll)                 // /researcher/Hardware
 	res.Get("/Security", handlers.ResearchersAll)                 // /researcher/Security
+
+	adm := app.Group("/admin", sessionControl)
+	adm.Get("/", handlers.AdminPage)
+	adm.Get("/comment", handlers.Comment)
+	adm.Post("/delete/:key", handlers.DeletUser)
+	adm.Post("/edit/:key", handlers.UpdateUser)
+	adm.Post("/create", handlers.CreateUser)
+	adm.Get("/galleryAdd", handlers.GalleryAddGet)
+	adm.Post("/galleryAdd", handlers.GalleryAddPost)
+	adm.Get("/user/:key", handlers.GetUser)
+	adm.Post("/user/update/:key", handlers.UpdateUser)
+	adm.Get("/user/delete/:key", handlers.DeletUser)
+
+}
+
+func sessionControl(c *fiber.Ctx) error {
+	sess, err := handlers.SessionStore.Get(c)
+	if err != nil {
+		fmt.Println(err)
+	}
+	if sess.Get("temp") == nil {
+		return c.Redirect("/login")
+	}
+	return c.Next()
 
 }
